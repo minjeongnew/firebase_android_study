1d3ea9f25c13c6ed27abe8dda840c314c97463d1
otnaj <20171623sw@kookmin.ac.kr>
2020-11-19 16:37:21 +0900

    update loaded data

--- app/src/main/java/com/newfact/newfacts/MainActivity.java
+++ app/src/main/java/com/newfact/newfacts/MainActivity.java
@@ -6,9 +6,17 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity{
 
@@ -21,7 +29,11 @@ public class MainActivity extends AppCompatActivity{
     private FragmentHealthInfo fragmentHealthInfo = new FragmentHealthInfo();
     private FragmentControl fragmentControl = new FragmentControl();
 
-    private UserInfo test;
+
+    DatabaseReference mDBReference = null;
+    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+    UserInfo userInfo;
 
 
     @Override
@@ -29,6 +41,53 @@ public class MainActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
 //        setContentView(R.layout.activity_main);
         setContentView(R.layout.activity_main);
+
+
+        // 로그인 후 데이터 가져오기
+        userInfo = UserInfo.getInstance();
+        String user_id = user.getUid();
+        mDBReference = FirebaseDatabase.getInstance().getReference();// 파이어베이스 참조
+        final DatabaseReference userInfoRef = mDBReference.child("UserInfo").child(user_id);
+        // 파이어베이스에서 유저 데이터 가져오기
+        userInfoRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                if(dataSnapshot.child("age").getValue()!=null){
+                    String age = dataSnapshot.child("age").getValue().toString();
+                    userInfo.setAge(age);
+                }
+                if(dataSnapshot.child("sex").getValue()!=null){
+                    String sex = dataSnapshot.child("sex").getValue().toString();
+                    userInfo.setSex(sex);
+                }
+                if(dataSnapshot.child("height").getValue()!=null){
+                    String height = dataSnapshot.child("height").getValue().toString();
+                    userInfo.setHeight(height);
+                }
+                if(dataSnapshot.child("weight").getValue()!=null){
+                    String weight = dataSnapshot.child("weight").getValue().toString();
+                    userInfo.setWeight(weight);
+                }
+                if (dataSnapshot.child("allergy").getValue() != null) {
+                    Log.d("firebase has data", "test");
+                    String allergy = dataSnapshot.child("allergy").getValue().toString();
+                    userInfo.setAllergy(allergy);
+                }
+                if (dataSnapshot.child("nutrition").getValue()!=null){
+                    String nutrition = dataSnapshot.child("nutrition").getValue().toString();
+                    userInfo.setNutrition(nutrition);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
+
 
         FragmentTransaction transaction = fragmentManager.beginTransaction();
         transaction.replace(R.id.frameLayout, fragmentMain).commitAllowingStateLoss();
